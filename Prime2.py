c409f0428e2a4a9fa6b0b70f24bcab0f9042ea97
ranjithsvcetcse <59575714+ranjithsvcetcse@users.noreply.github.com>
2020-01-27 23:17:57 +0530

    Prime2.py
    Find prime numbers between range of two given numbers.

---
+++ Prime2.py
@@ -0,0 +1,20 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def is_prime(n):
+    c=0
+    for i in range(1,n+1):
+        if n%i==0:
+            c=c+1
+        if c==3:
+            return False
+    if c==2:
+        return True
+    else:
+        return 'Error'
+
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    if is_prime(i):
+        print(i,end=" ")
+    else:
+        pass
